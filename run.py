7705dca51e1a90d6f7fd65c19069597f08f50a67
Michael FORASTE <michael.foraste@gmail.com>
2014-10-07 18:41:50 +0200

    Adding argument parsing

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-import launcher.finder
-
-path = os.path.dirname(os.path.realpath(__file__))
-modules = os.path.realpath(path + "/platform")
-
-launcher.finder.load(modules)
