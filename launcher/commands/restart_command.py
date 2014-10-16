b8552ee4c8494efa51a42cbe8c8edecb60a36f39
Michael FORASTE <michael.foraste@gmail.com>
2014-10-16 14:56:19 +0200

    Adding restart command

---
+++ launcher/commands/restart_command.py
@@ -0,0 +1,36 @@
+from base_command import BaseCommand
+from stop_command import StopCommand
+from run_command import RunCommand
+
+class RestartCommand(BaseCommand):
+	""" Stop containers """
+
+	def __init__(self):
+		super(RestartCommand, self).__init__()
+		self.main_command = "docker stop"
+
+	def execute(self, container):
+		"""
+			Restart container if running, depend on 2 commands :
+				- StopCommand : to stop container
+				- RunCommand : to start container
+		"""
+
+		## First, we stop the container
+		stop_command = StopCommand()
+		return_value = stop_command.execute(container)
+		
+		## Something happend while stopping container
+		## so we need to stop here
+		if return_value:
+			print "Oops, seems like we can't stop container"
+			return self.RETURN_WONT_DO
+
+		# refreshing container status
+		container.refresh_status()
+		
+		## Everything's fine, let's start this again
+		run_command = RunCommand()
+		run_command.execute(container)	
+		
+		return self.RETURN_SUCCESS
