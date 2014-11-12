887c6aed1274a50fd07137f0f9df1a52f22611e2
Michael FORASTE <michael.foraste@gmail.com>
2014-11-12 13:42:28 +0100

    creating data image type

---
+++ launcher/container/data.py
@@ -0,0 +1,12 @@
+from base_container import BaseContainer
+
+class Data(BaseContainer):
+	"""
+		A simple Data persistent image, we can build the image, but not run it.
+		Furthermore, you must confirm instance deletion with --with-data option
+	"""
+
+	def __init__(self, path, configuration = None):
+		super(Data, self).__init__(path, configuration)
+		self.runnable = False
+		self.deletable = False
